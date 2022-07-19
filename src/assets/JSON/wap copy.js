c5debf08f9ed6e9eb1e293c396d47a9462beb227
Guy Kadosh <guyk50@gmail.com>
2022-07-20 00:07:47 +0300

    updated JSON

--- src/assets/JSON/wap copy.js
+++ src/assets/JSON/wap copy.js
@@ -1,70 +1,111 @@
 // Tips & Tricks
 // No need for another entity: Template, just use isPublic on the wap
 // Focus on make it easy to demo over supporting full customization
-// Support adding full-blown-component rather than letting the user add stuff manually 
+// Support adding full-blown-component rather than letting the user add stuff manually
 // (Delete is easy to implement later)
 // Support several themes for components (CSS only)
 // default FLEX is your friend, handle media proprtion correctly
 // Here are some components:
 // wap-header, wap-footer, wap-container
 // wap-gallery (photos: {url, title,txt})
-// wap-map (center,zoom,markers), 
-// wap-video (url, iframe) 
-// wap-social (socials) 
+// wap-map (center,zoom,markers),
+// wap-video (url, iframe)
+// wap-social (socials)
 // wap-signup => wapService
 // wap-chat
 // wap-contact-us => wapService
 
 var wap = {
-	"_id": "5e28393890dd7201a06d4e44",
-	"name": "HairDresser Baluta Marketing Site",
-	"imgUrl": "http://res.cloudinary.com/webify/image/upload/v1580021948/coffe_yi0yzf.png",
-	"createdBy": {
-		"_id": "5e26e0b718a0891d4c995527",
-		"username": "Hetro Sekshual"
-	},
-	"cmps": [
-		{
-			"id": "wc02",
-			"type": "wap-header",
-			"info": {
-				"title": "HairDresser Baluta",
-				"substitle": "Your Hair is !(Who you Are)",
-				"btn": {"label": "Schedule Today!", "link" : "#wc03"}
-			},
-			"style": {
-				"background": "url()",
-				"padding": "10px"
-			}
-		},
-		{
-			"id": "wc01",
-			"type": "wap-map",
-			"info": {
-				"name": "Paris",
-				"lat": 12.909,
-				"lng": 23.9,
-				"zoom": 2
-			},
-			"style": {
-				"backgroundColor": "#f53b76",
-				"height": "300px",
-				"padding": "10px"
-			}
-		},		
-		{
-			"id": "wc03",
-			"type": "wap-container",
-			"info": {
-				"dir": "column",
-				"cmps": [
-					{},
-					{}
-				]
-			},
-			"style": {
-			}
-		}
-	],
-	"isPublic" : true
+  _id: '5e28393890dd7201a06d4e44',
+  name: 'HairDresser Baluta Marketing Site',
+  imgUrl:
+    'http://res.cloudinary.com/webify/image/upload/v1580021948/coffe_yi0yzf.png',
+  createdBy: {
+    _id: '5e26e0b718a0891d4c995527',
+    username: 'Hetro Sekshual',
+  },
+  cmps: [
+    {
+      id: 'wc02',
+      type: 'wap-header',
+      info: {
+        title: 'HairDresser Baluta',
+        substitle: 'Your Hair is !(Who you Are)',
+        btn: { label: 'Schedule Today!', link: '#wc03' },
+      },
+      style: {
+        background: 'url()',
+        padding: '10px',
+      },
+    },
+    {
+      id: 'wc01',
+      type: 'wap-map',
+      info: {
+        name: 'Paris',
+        lat: 12.909,
+        lng: 23.9,
+        zoom: 2,
+      },
+      style: {
+        backgroundColor: '#f53b76',
+        height: '300px',
+        padding: '10px',
+      },
+    },
+    {
+      id: 'wc03',
+      type: 'wap-container',
+      info: {
+        dir: 'column',
+        cmps: [{}, {}],
+      },
+      style: {},
+    },
+  ],
+  isPublic: true,
 }
+
+// Tips & Tricks
+// Use the isPublic to allow cloning the wap
+// Focus on make it easy to demo over supporting full customization
+// Support adding full-blown-component rather than letting the user add stuff manually
+// (Delete is easy to implement later)
+// Support several themes for components (CSS only)
+// default FLEX is your friend, handle media proprtion correctly
+// Here are some components:
+// wap-header, wap-footer, wap-container
+// wap-gallery (photos: {url, title,txt})
+// wap-map (center,zoom,markers),
+// wap-video (url, iframe)
+// wap-social (socials)
+// wap-signup => wapService
+// wap-chat
+// wap-contact-us => wapService
+// wap-navbar
+
+// Edit Mode, two options:
+// wap-header-edit  / wap-header
+// wap-social-edit  / wap-social
+
+// Split work
+// wap-details : render the predefined JSON
+// wap-header, wap-map (dummy - map-photo)
+// wap-container
+
+// wap-edit
+// wap-header-edit, wap-chat-edit (config - bot delay time)
+// edit...
+// Adding a component:
+// *. User select WapCmp from <select>
+// *. WapCmp is added at the bottom
+// *. Let the user <select> After which WapCmp it should be added
+// *. add <select> for theme (themes are hard coded in wapService.getThemesFor(wapCmp.type))
+// Save button
+// Homepage
+// render 3 waps links
+// support clone
+// reset button?
+
+// DASHBOARD
+// visits, contact-us, signups
--- src/assets/JSON/wap.js
+++ src/assets/JSON/wap.js
@@ -1,55 +1,11 @@
-// Tips & Tricks
-// Use the isPublic to allow cloning the wap
-// Focus on make it easy to demo over supporting full customization
-// Support adding full-blown-component rather than letting the user add stuff manually
-// (Delete is easy to implement later)
-// Support several themes for components (CSS only)
-// default FLEX is your friend, handle media proprtion correctly
-// Here are some components:
-// wap-header, wap-footer, wap-container
-// wap-gallery (photos: {url, title,txt})
-// wap-map (center,zoom,markers),
-// wap-video (url, iframe)
-// wap-social (socials)
-// wap-signup => wapService
-// wap-chat
-// wap-contact-us => wapService
-// wap-navbar
-
-// Edit Mode, two options:
-// wap-header-edit  / wap-header
-// wap-social-edit  / wap-social
-
-// Split work
-// wap-details : render the predefined JSON
-// wap-header, wap-map (dummy - map-photo)
-// wap-container
-
-// wap-edit
-// wap-header-edit, wap-chat-edit (config - bot delay time)
-// edit...
-// Adding a component:
-// *. User select WapCmp from <select>
-// *. WapCmp is added at the bottom
-// *. Let the user <select> After which WapCmp it should be added
-// *. add <select> for theme (themes are hard coded in wapService.getThemesFor(wapCmp.type))
-// Save button
-// Homepage
-// render 3 waps links
-// support clone
-// reset button?
-
-// DASHBOARD
-// visits, contact-us, signups
-
 var wap = {
   _id: '5e28393890dd7201a06d4e44',
-  name: 'HairDresser Baluta Marketing Site',
+  name: 'Lukas Rudrof – Funktionales Webdesign, Webflow & SEO',
   imgUrl:
     'http://res.cloudinary.com/webify/image/upload/v1580021948/coffe_yi0yzf.png',
   createdBy: {
     _id: '5e26e0b718a0891d4c995527',
-    fullname: 'Hekro Special',
+    fullname: 'Lukas Rudrof',
     imgUrl: 'img.jpg',
   },
   usersData: {
@@ -58,44 +14,128 @@ var wap = {
     ],
     signups: [{ email: 'user@user.com', at: 123 }],
   },
+  theme: 'vivid',
   cmps: [
     {
-      id: 'wc02',
+      _id: 'wc02',
       type: 'wap-header',
+      classes: [
+        'omnifood-header',
+        'flex',
+        'justify-between',
+        'full',
+        'main-layout',
+      ],
       info: {
-        title: 'HairDresser Baluta',
-        substitle: 'Your Hair is !(Who you Are)',
-        btn: { label: 'Schedule Today!', link: '#wc03' },
+        logo: {
+          img: '',
+          title: '',
+          style: {},
+        },
+        nav: {
+          _id: 'wc05',
+          type: 'wap-nav',
+          info: {
+            links: [
+              { title: 'How it works', href: '#how' },
+              { title: 'Meals', href: '#meals' },
+              { title: 'Reviews', href: '#reviews' },
+              { title: 'Pricing', href: '#pricing' },
+              { title: 'Try for free', href: '#cta' },
+            ],
+          },
+          style: {},
+        },
       },
-      theme: 'theme-header-happy',
-      style: {
-        background: 'url()',
-        font: 'Fontush',
-        color: 'red',
-      },
+      style: {},
     },
     {
       id: 'wc01',
-      type: 'wap-map',
+      type: 'wap-hero',
+      classes: ['omnifood-hero'],
       info: {
-        name: 'Paris',
-        lat: 12.909,
-        lng: 23.9,
-        zoom: 2,
+        heading: {
+          title: 'A healthy meal delivered to your door, every single day',
+          style: {},
+        },
+        'sub-heading': {
+          title:
+            'The smart 365-days-per-year food subscription that will make you eat healthy again. Tailored to your personal tastes and nutritional needs.',
+          style: {},
+        },
+        btns: [
+          { label: 'Start Eating Well!', link: '#wc03' },
+          { label: 'Learn more', link: '#wc03' },
+        ],
+        imgUrl: 'hero-img.jpg',
       },
-      theme: 'theme-map-exciting',
-      style: {
-        height: '300px',
+      style: {},
+    },
+    {
+      id: 'wc04',
+      classes: ['omnifood-brand', 'flex'],
+      type: 'wap-imgs',
+      info: {
+        title: 'as featured in',
+        imgs: ['brandname1.jpg', 'brandname2.jpg'],
       },
+      style: {},
     },
     {
       id: 'wc03',
       type: 'wap-container',
       info: {
         dir: 'row',
-        cmps: [{}, {}],
+        cmps: [
+          {
+            id: 'wc04',
+            classes: ['omnifood-brand', 'flex'],
+            type: 'wap-card',
+            info: {
+              num: '',
+              imgUrl: '',
+              heading: '',
+              subheading: '',
+              list: [
+                { icon: '', txt: '' },
+                { icon: '', txt: '' },
+                { icon: '', txt: '' },
+              ],
+            },
+            style: {},
+          },
+          ,
+          {
+            id: 'wc04',
+            classes: ['omnifood-brand', 'flex'],
+            type: 'wap-card',
+            info: {
+              num: '',
+              imgUrl: '',
+              heading: '',
+              subheading: '',
+              list: [
+                { icon: '', txt: '' },
+                { icon: '', txt: '' },
+                { icon: '', txt: '' },
+              ],
+            },
+            style: {},
+          },
+          {
+            id: 'wc04',
+            classes: ['omnifood-brand', 'flex'],
+            type: 'wap-list',
+            info: {
+              'list-icon': '',
+              list: ['Vegeterian', 'Vegan'],
+              link: '',
+            },
+            style: {},
+          },
+          ,
+        ],
       },
-      theme: 'theme-container-base',
       style: {},
     },
   ],
