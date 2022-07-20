0eb01c7f735c38d28a740b1897e86f6625a60e27
Denis Lit <denislitviak@gmail.com>
2022-07-20 12:16:15 +0300

    delete spaces

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template>
-  <section>
-    <pre>{{ showWaps }}</pre>
-  </section>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data() {
-    return {
-
-    }
-  },
   created() {
     this.$store.dispatch({ type: 'loadWaps' })
-  },
-  computed: {
-      showWaps() {
-      return this.$store.getters.getWaps
-    }
-  },
-  methods: {},
-  components: {
-  
   },
 }
 </script>
