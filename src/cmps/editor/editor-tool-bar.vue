e40772c45777e5d97f0403c6bb545051c79a519e
Denis Lit <denislitviak@gmail.com>
2022-07-20 15:39:32 +0300

    add tool bar to editor, give basic styles

---
+++ src/cmps/editor/editor-tool-bar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="edit-tool-bar flex">
+
+        <ul class="tool-bar-nav">
+            <li>Elements</li>
+            <li>Sections</li>
+            <li>Editor</li>
+        </ul>
+
+        <section class="tools">
+            <div class="cmp-preview" v-for="cmp in cmps">
+                <span>{{ cmp.type }}</span>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'edit-tool-bar',
+    props: {
+        cmps: Array
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    components: {
+
+    },
+}
+</script>
+
+<style>
+</style>
