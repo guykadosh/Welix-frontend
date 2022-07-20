99ccae62c7c9275ee479f33a77f7932551b6eb1b
Guy Kadosh <guyk50@gmail.com>
2022-07-20 16:22:36 +0300

    updated json and stater desgin omnifood hero

--- src/cmps/waps/wap-hero.vue
+++ src/cmps/waps/wap-hero.vue
@@ -1,19 +1,21 @@
 <template>
   <section v-if="cmp" class="wap-hero" :style="cmp.style" :class="cmp.classes">
-    <div class="text-box">
-      <h1 :style="info.heading.style">
-        {{ info.heading.title }}
-      </h1>
-      <h2 :style="info.subHeading.style">
-        {{ info.subHeading.title }}
-      </h2>
-      <div class="btns">
-        <a class="btn" v-for="btn in info.btns" :href="btn.link">
-          {{ btn.label }}</a
-        >
+    <div class="hero-inner">
+      <div class="text-box">
+        <h1 :style="info.heading.style">
+          {{ info.heading.title }}
+        </h1>
+        <h2 :style="info.subHeading.style">
+          {{ info.subHeading.title }}
+        </h2>
+        <div class="btns">
+          <a class="btn" v-for="btn in info.btns" :href="btn.link">
+            {{ btn.label }}</a
+          >
+        </div>
       </div>
+      <img v-if="info.imgUrl" :src="info.imgUrl" />
     </div>
-    <img v-if="info.imgUrl" :src="info.imgUrl" />
   </section>
 </template>
 <script>
