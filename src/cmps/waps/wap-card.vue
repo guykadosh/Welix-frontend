2563d8225b37cc12ad761c79d8b4e71d87c71812
Guy Kadosh <guyk50@gmail.com>
2022-07-20 18:51:37 +0300

    updated json, card and design

--- src/cmps/waps/wap-card.vue
+++ src/cmps/waps/wap-card.vue
@@ -1,26 +1,29 @@
 <template>
-    <section v-if="cmp" class="wap-card" :class="cmp.classes">
-        <h2 v-if="info.heading">{{ info.heading }}</h2>
-        <p v-if="info.subHeading">{{ info.subHeading }}</p>
-        <ul class="card-list clean-list">
-            <li v-for="line in info.list" :key="line">
-                <span>{{line.icon}}</span> <span>{{line.txt}}</span>
-            </li>
-        </ul>
-        <img :src="info.imgUrl" alt="card-img">
-
-    </section>
+  <section v-if="cmp" class="wap-card" :class="cmp.classes">
+    <img :src="info.imgUrl" alt="card-img" />
+    <div class="card-content">
+      <h2 class="card-heading" v-if="info.heading">{{ info.heading }}</h2>
+      <h3 class="card-subheading" v-if="info.subHeading">
+        {{ info.subHeading }}
+      </h3>
+      <ul class="card-list clean-list">
+        <li v-for="line in info.list" :key="line">
+          <span>{{ line.icon }}</span> <span>{{ line.txt }}</span>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 <script>
 export default {
-    name: 'wap-card',
-    props: {
-        cmp: Object
+  name: 'wap-card',
+  props: {
+    cmp: Object,
+  },
+  computed: {
+    info() {
+      return this.cmp.info
     },
-    computed: {
-        info() {
-            return this.cmp.info
-        }
-    }
+  },
 }
 </script>
