9fdc0a27203471948c335530755e0ff3e5df0f20
Denis Lit <denislitviak@gmail.com>
2022-07-20 23:05:06 +0300

    add wap cmps to editor

--- src/views/wap-editor.vue
+++ src/views/wap-editor.vue
@@ -15,6 +15,14 @@ import editorHeader from '../cmps/editor/editor-header.vue'
 import editorNav from '../cmps/editor/editor-nav.vue'
 import editorToolBar from '../cmps/editor/editor-tool-bar.vue'
 import wapPreview from '../cmps/editor/wap-preview.vue'
+import wapGallery from '../cmps/waps/wap-gallery.vue'
+import wapList from '../cmps/waps/wap-list.vue'
+import wapNav from '../cmps/waps/wap-nav.vue'
+import wapText from '../cmps/waps/wap-text.vue'
+import wapReview from '../cmps/waps/wap-review.vue'
+import wapFooter from '../cmps/waps/wap-footer.vue'
+import wapCard from '../cmps/waps/wap-card.vue'
+import wapContainer from '../cmps/waps/wap-container.vue'
 
 export default {
     name: 'wap-editor',
@@ -38,7 +46,15 @@ export default {
         editorHeader,
         editorNav,
         editorToolBar,
-        wapPreview
+        wapPreview,
+        wapGallery,
+        wapList,
+        wapNav,
+        wapText,
+        wapReview,
+        wapFooter,
+        wapCard,
+        wapContainer
     },
 }
 </script>
