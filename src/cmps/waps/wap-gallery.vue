88d1e48225edf5095d69d4e7eb1db153e7b70b03
Guy Kadosh <guyk50@gmail.com>
2022-07-20 18:22:43 +0300

    omnifood-brand design and json update

--- src/cmps/waps/wap-gallery.vue
+++ src/cmps/waps/wap-gallery.vue
@@ -1,23 +1,21 @@
 <template>
-    <section v-if="cmp" class="wap-gallery" :class="cmp.classes">
-        <h3>{{ info.title }}</h3>
-        <div v-for="img in info.imgs" :key="img">
-            <img :src="img" alt="gallery-img">
-        </div>
-    </section>
+  <section v-if="cmp" class="wap-gallery" :class="cmp.classes">
+    <h3 class="title">{{ info.title }}</h3>
+    <div class="imgs-container">
+      <img v-for="img in info.imgs" :key="img" :src="img" alt="gallery-img" />
+    </div>
+  </section>
 </template>
 <script>
-
-
 export default {
-    name: 'wap-gallery',
-    props: {
-        cmp: Object
+  name: 'wap-gallery',
+  props: {
+    cmp: Object,
+  },
+  computed: {
+    info() {
+      return this.cmp.info
     },
-    computed: {
-        info() {
-            return this.cmp.info
-        }
-    }
+  },
 }
 </script>
