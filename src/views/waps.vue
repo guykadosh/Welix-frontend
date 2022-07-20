fe7b99ada0990685111acecf4a29a89f5c7d1bb7
Yuval <yuval.shmukler@gmail.com>
2022-07-20 22:48:21 +0300

    waps container

--- src/views/waps.vue
+++ src/views/waps.vue
@@ -1,8 +1,37 @@
 <template>
-    <section>
+    <section class="waps-container main-layout">
+
         <ul>
-            <li><router-link to="/wap/editor/5e28393890dd7201a06d4e44">Template 1</router-link></li>
+            <li>
+                <router-link to="/wap/editor/5e28393890dd7201a06d4e44">Template 1</router-link>
+            </li>
         </ul>
+        <div class="img-container">
+            <div>
+                <img src="@/assets/img/ominfood.png" alt="" />
+                <p>Healthy food</p>
+            </div>
+            <div>
+                <img src="@/assets/img/ominfood.png" alt="" />
+                <p>Healthy food</p>
+            </div>
+            <div>
+                <img src="@/assets/img/ominfood.png" alt="" />
+                <p>Healthy food</p>
+            </div>
+            <div>
+                <img src="@/assets/img/ominfood.png" alt="" />
+                <p>Healthy food</p>
+            </div>
+            <div>
+                <img src="@/assets/img/ominfood.png" alt="" />
+                <p>Healthy food</p>
+            </div>
+            <div>
+                <img src="@/assets/img/ominfood.png" alt="" />
+                <p>Healthy food</p>
+            </div>
+        </div>
     </section>
 </template>
 
