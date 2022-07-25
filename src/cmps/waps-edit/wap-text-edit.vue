0d5de423c0c25a1685cf8c823dade47d680afe32
Yuval <yuval.shmukler@gmail.com>
2022-07-25 11:10:35 +0300

    JSON

--- src/cmps/waps-edit/wap-text-edit.vue
+++ src/cmps/waps-edit/wap-text-edit.vue
@@ -1,41 +1,13 @@
 <template>
-  <section
-    v-if="cmp"
-    class="wap-text"
-    :class="cmp.classes"
-    :style="cmp.style"
-    @click="setEditable('cmp')"
-  >
-    <span
-      class="tag"
-      v-if="info.tag"
-      :style="info.tag.style"
-      ref="tag"
-      @click.stop="setEditable(info.tag.type, 'tag')"
-      @input="changeTxt('tag')"
-      @mousedown.stop
-      >{{ info.tag.txt }}</span
-    >
-    <h2
-      contenteditable="true"
-      class="text1"
-      :style="info.heading.style"
-      ref="heading"
-      @click.stop="setEditable(info.heading.type, 'heading')"
-      @input="changeTxt('heading')"
-      @mousedown.stop
-    >
+  <section v-if="cmp" class="wap-text" :class="cmp.classes" :style="cmp.style" @click="setEditable('cmp')">
+    <span class="tag" v-if="info.tag" :style="info.tag.style" ref="tag" @click.stop="setEditable(info.tag.type, 'tag')"
+      @input="changeTxt('tag')" @mousedown.stop>{{ info.tag.txt }}</span>
+    <h2 contenteditable="true" class="text1" :style="info.heading.style" ref="heading"
+      @click.stop="setEditable(info.heading.type, 'heading')" @input="changeTxt('heading')" @mousedown.stop>
       {{ info.heading.txt }}
     </h2>
-    <h2
-      contenteditable="true"
-      class="text2"
-      ref="subHeading"
-      :style="info.subHeading.style"
-      @click.stop="setEditable(info.subHeading.type, 'subHeading')"
-      @input="changeTxt('subHeading')"
-      @mousedown.stop
-    >
+    <h2 v-if="info.subHeading" contenteditable="true" class="text2" ref="subHeading" :style="info.subHeading.style"
+      @click.stop="setEditable(info.subHeading.type, 'subHeading')" @input="changeTxt('subHeading')" @mousedown.stop>
       {{ info.subHeading.txt }}
     </h2>
   </section>
