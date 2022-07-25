856d8c0ec416754668833cf17cc5b56f68f2aeca
Yuval <yuval.shmukler@gmail.com>
2022-07-25 14:39:36 +0300

    design web and fixed layout

--- src/cmps/waps-edit/wap-map-edit.vue
+++ src/cmps/waps-edit/wap-map-edit.vue
@@ -1,10 +1,13 @@
 <template>
-    <GMapMap class="map small" :center="cmp.info.center" :style="mapSize" :zoom="15" map-type-id="terrain" @mousedown.stop >
-        <GMapCluster @mousedown.stop>
-            <GMapMarker :key="index" v-for="(m, index) in cmp.info.markers" :position="m.position" :clickable="false"
-                :draggable="false" @click="center = m.position" @mousedown.stop/>
-        </GMapCluster>
-    </GMapMap>
+    <div class="map-container">
+        <GMapMap class="map small" :center="cmp.info.center" :style="mapSize" :zoom="15" map-type-id="terrain"
+            @mousedown.stop>
+            <GMapCluster @mousedown.stop>
+                <GMapMarker :key="index" v-for="(m, index) in cmp.info.markers" :position="m.position"
+                    :clickable="false" :draggable="false" @click="center = m.position" @mousedown.stop />
+            </GMapCluster>
+        </GMapMap>
+    </div>
 </template>
 
 <script>
