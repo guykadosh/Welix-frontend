cd8b8fca4fc0cb6680b217b755a935a007f866f8
Denis Lit <denislitviak@gmail.com>
2022-07-27 09:42:58 +0300

    finish section 2 home

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,12 @@
 <template>
   <section class="home-page">
-    <!-- <div class="bg-cover"></div> -->
-    <div class="top-wrapper full main-layout ">
+    <div class="bg-cover"></div>
+    <div class="top-wrapper full main-layout">
       <app-header />
       <hero />
     </div>
     <editor-preview />
+    <waps-carousel/>
   </section>
 </template>
 
@@ -13,13 +14,15 @@
 import appHeader from '../cmps/app/app-header.vue'
 import hero from '../cmps/app/hero.vue'
 import editorPreview from '../cmps/app/editor-preview.vue'
+import wapsCarousel from '../cmps/app/waps-carousel.vue'
 export default {
   name: '',
   components: {
     appHeader,
     hero,
     editorPreview,
-  },
+    wapsCarousel
+},
 }
 </script>
 
