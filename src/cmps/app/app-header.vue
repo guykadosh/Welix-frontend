86e61accba1077162aca75f7be27e406193a82a6
Guy Kadosh <guyk50@gmail.com>
2022-07-28 08:26:58 +0300

    logo and fav icon

--- src/cmps/app/app-header.vue
+++ src/cmps/app/app-header.vue
@@ -1,23 +1,39 @@
 <template>
   <header class="main-header main-layout full">
-     
     <div class="header__inner flex justify-between items-center">
-
       <section class="logo-wrapper">
-        <h2 class="main-header-logo" @click="$router.push('/')">Welix</h2>
+        <!-- <h2 class="main-header-logo" @click="$router.push('/')">Welix</h2> -->
+        <img
+          class="logo"
+          src="@/assets/img/logo.png"
+          alt=""
+          @click="$router.push('/')"
+        />
       </section>
 
       <section class="nav-wrapper">
-        <router-link class="header-link link-mid" to="/template">Templates</router-link>
-        <router-link class="header-link link-mid" to="/wap/editor/">Editor</router-link>
+        <router-link class="header-link link-mid" to="/template"
+          >Templates</router-link
+        >
+        <router-link class="header-link link-mid" to="/wap/editor/"
+          >Editor</router-link
+        >
       </section>
 
       <section class="login-wrapper">
-        <router-link class="header-link link-dashboard" v-if="isUser" to="/dashboard">backoffice</router-link>
-        <router-link class="header-link link-login" v-if="!isUser" to="/login">log in</router-link>
-        <router-link class="header-link link-signup" v-if="!isUser" to="/login">sign up</router-link>
+        <router-link
+          class="header-link link-dashboard"
+          v-if="isUser"
+          to="/dashboard"
+          >backoffice</router-link
+        >
+        <router-link class="header-link link-login" v-if="!isUser" to="/login"
+          >log in</router-link
+        >
+        <router-link class="header-link link-signup" v-if="!isUser" to="/login"
+          >sign up</router-link
+        >
       </section>
-
     </div>
   </header>
 </template>
