9426d939a3fd1f1e48a304b5c28c9ce9879cadfc
Guy Kadosh <guyk50@gmail.com>
2022-07-28 15:55:37 +0300

    cake footer fix

--- src/cmps/waps/wap-map.vue
+++ src/cmps/waps/wap-map.vue
@@ -1,27 +1,39 @@
 <template>
-    <div class="map-container">
-        <GMapMap class="map small" :center="cmp.info.center" :style="mapSize" :zoom="15" map-type-id="terrain"
-            @mousedown.stop>
-            <GMapCluster @mousedown.stop>
-                <GMapMarker :key="index" v-for="(m, index) in cmp.info.markers" :position="m.position"
-                    :clickable="false" :draggable="false" @click="center = m.position" @mousedown.stop />
-            </GMapCluster>
-        </GMapMap>
-    </div>
+  <div class="map-container">
+    <GMapMap
+      class="map small"
+      :center="cmp.info.center"
+      :style="mapSize"
+      :zoom="15"
+      map-type-id="terrain"
+      @mousedown.stop
+    >
+      <GMapCluster @mousedown.stop>
+        <GMapMarker
+          :key="index"
+          v-for="(m, index) in cmp.info.markers"
+          :position="m.position"
+          :clickable="false"
+          :draggable="false"
+          @click="center = m.position"
+          @mousedown.stop
+        />
+      </GMapCluster>
+    </GMapMap>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'cmp-map',
-    props: { cmp: Object },
-    computed: {
-        mapSize() {
-            return { width: 500 + 'px', height: 450 + 'px' }
-        }
+  name: 'cmp-map',
+  props: { cmp: Object },
+  computed: {
+    mapSize() {
+      return { width: 500 + 'px', height: 450 + 'px' }
     },
-    created() {
-        console.log('im map')
-    },
+  },
+  created() {
+    console.log('im map')
+  },
 }
 </script>
-
