47e96b51ae92d253d7f1bb7199e5529fe2d9744c
Guy Kadosh <guyk50@gmail.com>
2022-08-01 19:01:27 +0300

    fixed wap-text

--- src/cmps/waps-edit/wap-text-edit.vue
+++ src/cmps/waps-edit/wap-text-edit.vue
@@ -1,26 +1,60 @@
 <template>
-  <section v-if="cmp" class="wap-text" :style="cmp.style" :class="[...cmp.classes, selected]" v-click-outside="unselect"
-    @click="setEditable('cmp')">
+  <section
+    v-if="cmp"
+    class="wap-text"
+    :style="cmp.style"
+    :class="[...cmp.classes, selected]"
+    v-click-outside="unselect"
+    @click="setEditable('cmp')"
+  >
     <section class="__inner">
-      <span class="tag" v-if="info.tag" :style="info.tag.style" ref="tag"
-        @click.stop="setEditable(info.tag.type, 'tag')" @input="changeTxt('tag')" @mousedown.stop>{{ info.tag.txt
-        }}</span>
-      <h2 contenteditable="true" class="text1" :style="info.heading.style" ref="heading"
-        @click.stop="setEditable(info.heading.type, 'heading')" @input="changeTxt('heading')" @mousedown.stop>
+      <span
+        class="tag"
+        v-if="info.tag"
+        :style="info.tag.style"
+        ref="tag"
+        @click.stop="setEditable(info.tag.type, 'tag')"
+        @input="changeTxt('tag')"
+        @mousedown.stop
+        >{{ info.tag.txt }}</span
+      >
+      <h2
+        contenteditable="true"
+        class="text1"
+        :style="info.heading.style"
+        ref="heading"
+        @click.stop="setEditable(info.heading.type, 'heading')"
+        @input="changeTxt('heading')"
+        @mousedown.stop
+      >
         {{ info.heading.txt }}
       </h2>
-      <h2 v-if="info.subHeading" contenteditable="true" class="text2" ref="subHeading" :style="info.subHeading.style"
-        @click.stop="setEditable(info.subHeading.type, 'subHeading')" @input="changeTxt('subHeading')" @mousedown.stop>
+      <h2
+        v-if="info.subHeading"
+        contenteditable="true"
+        class="text2"
+        ref="subHeading"
+        :style="info.subHeading.style"
+        @click.stop="setEditable(info.subHeading.type, 'subHeading')"
+        @input="changeTxt('subHeading')"
+        @mousedown.stop
+      >
         {{ info.subHeading.txt }}
       </h2>
-      <div contenteditable="true" class="icon-wrapper" ref="subHeading">
-        <img :src="info.icon.src" alt=""   
-          :class="info.icon.classes" 
-          :style="info.icon.style" 
-          v-if="info.icon" 
-          :ref="`icon`"
-           @click.stop="setEditable(info.icon.type, 'icon')"
+      <div
+        v-if="info.icon"
+        contenteditable="true"
+        class="icon-wrapper"
+        ref="subHeading"
       >
+        <img
+          :src="info.icon.src"
+          alt=""
+          :class="info.icon.classes"
+          :style="info.icon.style"
+          :ref="`icon`"
+          @click.stop="setEditable(info.icon.type, 'icon')"
+        />
       </div>
     </section>
   </section>
